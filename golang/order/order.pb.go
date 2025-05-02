bb1fe6f1e93835ba4d507b72cdc327915b41edf4
morninng <mr_morninng_star_dash@yahoo.co.jp>
2025-05-02 16:57:39 +0900

    rename

--- golang/order/order.pb.go
+++ golang/order/order.pb.go
@@ -24,7 +24,7 @@ const (
 type CreateOrderRequest struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	UserId        int64                  `protobuf:"varint,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
-	Items         []*Item                `protobuf:"bytes,2,rep,name=items,proto3" json:"items,omitempty"`
+	OrderItems    []*OrderItem           `protobuf:"bytes,2,rep,name=order_items,json=orderItems,proto3" json:"order_items,omitempty"`
 	TotalPrice    float32                `protobuf:"fixed32,3,opt,name=total_price,json=totalPrice,proto3" json:"total_price,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
@@ -67,9 +67,9 @@ func (x *CreateOrderRequest) GetUserId() int64 {
 	return 0
 }
 
-func (x *CreateOrderRequest) GetItems() []*Item {
+func (x *CreateOrderRequest) GetOrderItems() []*OrderItem {
 	if x != nil {
-		return x.Items
+		return x.OrderItems
 	}
 	return nil
 }
@@ -81,27 +81,27 @@ func (x *CreateOrderRequest) GetTotalPrice() float32 {
 	return 0
 }
 
-type Item struct {
+type OrderItem struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	Name          string                 `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
 
-func (x *Item) Reset() {
-	*x = Item{}
+func (x *OrderItem) Reset() {
+	*x = OrderItem{}
 	mi := &file_order_order_proto_msgTypes[1]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
 
-func (x *Item) String() string {
+func (x *OrderItem) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*Item) ProtoMessage() {}
+func (*OrderItem) ProtoMessage() {}
 
-func (x *Item) ProtoReflect() protoreflect.Message {
+func (x *OrderItem) ProtoReflect() protoreflect.Message {
 	mi := &file_order_order_proto_msgTypes[1]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -113,12 +113,12 @@ func (x *Item) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use Item.ProtoReflect.Descriptor instead.
-func (*Item) Descriptor() ([]byte, []int) {
+// Deprecated: Use OrderItem.ProtoReflect.Descriptor instead.
+func (*OrderItem) Descriptor() ([]byte, []int) {
 	return file_order_order_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *Item) GetName() string {
+func (x *OrderItem) GetName() string {
 	if x != nil {
 		return x.Name
 	}
@@ -173,13 +173,15 @@ var File_order_order_proto protoreflect.FileDescriptor
 
 const file_order_order_proto_rawDesc = "" +
 	"\n" +
-	"\x11order/order.proto\"k\n" +
+	"\x11order/order.proto\"{\n" +
 	"\x12CreateOrderRequest\x12\x17\n" +
-	"\auser_id\x18\x01 \x01(\x03R\x06userId\x12\x1b\n" +
-	"\x05items\x18\x02 \x03(\v2\x05.ItemR\x05items\x12\x1f\n" +
+	"\auser_id\x18\x01 \x01(\x03R\x06userId\x12+\n" +
+	"\vorder_items\x18\x02 \x03(\v2\n" +
+	".OrderItemR\n" +
+	"orderItems\x12\x1f\n" +
 	"\vtotal_price\x18\x03 \x01(\x02R\n" +
-	"totalPrice\"\x1a\n" +
-	"\x04Item\x12\x12\n" +
+	"totalPrice\"\x1f\n" +
+	"\tOrderItem\x12\x12\n" +
 	"\x04name\x18\x01 \x01(\tR\x04name\"0\n" +
 	"\x13CreateOrderResponse\x12\x19\n" +
 	"\border_id\x18\x01 \x01(\x05R\aorderId2<\n" +
@@ -201,11 +203,11 @@ func file_order_order_proto_rawDescGZIP() []byte {
 var file_order_order_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_order_order_proto_goTypes = []any{
 	(*CreateOrderRequest)(nil),  // 0: CreateOrderRequest
-	(*Item)(nil),                // 1: Item
+	(*OrderItem)(nil),           // 1: OrderItem
 	(*CreateOrderResponse)(nil), // 2: CreateOrderResponse
 }
 var file_order_order_proto_depIdxs = []int32{
-	1, // 0: CreateOrderRequest.items:type_name -> Item
+	1, // 0: CreateOrderRequest.order_items:type_name -> OrderItem
 	0, // 1: Order.Create:input_type -> CreateOrderRequest
 	2, // 2: Order.Create:output_type -> CreateOrderResponse
 	2, // [2:3] is the sub-list for method output_type
